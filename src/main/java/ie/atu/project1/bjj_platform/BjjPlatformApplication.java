976547cc16666f9dcc299146d8c2b692c4e7abc0
G00405014 <G00405014@atu.ie>
2024-01-08 18:26:01 +0000

    Added Dependencies

---
+++ src/main/java/ie/atu/project1/bjj_platform/BjjPlatformApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.project1.bjj_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BjjPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BjjPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/project1/bjj_platform/BjjPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.project1.bjj_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BjjPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
